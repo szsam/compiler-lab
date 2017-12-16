0fe9450096def3f97f73e4587081ccabc9315456
151220091-Shen MingJie <151220091@smail.nju.edu.cn>
2017-12-16 23:31:55 +0800

    Refactor symbol table

--- src/Env.h
+++ src/Env.h
@@ -1,32 +1,52 @@
 #pragma once
 
 #include <string>
-#include <map>
+#include <unordered_map>
 #include <memory>
+#include <list>
 
-// #include "Type.h"
-struct Type;
-
-struct Env
+#include "Type.h"
+// struct Type;
+struct Symbol
 {
-	std::map<std::string,
-			 std::shared_ptr<Type> > table;
-	std::shared_ptr<Env> prev;
+	std::shared_ptr<Type> type;
+	// variable number, used in intermediate code
+	int var_no;
+
+	Symbol() : type(), var_no(-1) {}
+	Symbol(std::shared_ptr<Type> t) : type(t), var_no(-1) {}
+	Symbol(std::shared_ptr<Type> t, int no) : type(t), var_no(no) {}
+};
+
+class SymbolTable
+{
+private:
+	std::list< std::unordered_map<std::string, Symbol> > tables;
+	int variable_no = 0;
 
 	// methods
-	Env(std::shared_ptr<Env> p) : prev(p) {}	
 
-	bool put(std::string name, std::shared_ptr<Type> type)
+public:
+	void enter_scope() { tables.emplace_back(); }
+	void exit_scope() { tables.pop_back(); }
+
+	bool put(const std::string &name, std::shared_ptr<Type> type)
 	{
-		return table.insert({name, type}).second;
+		Symbol sym(type);
+		if (type->is_basic()) 
+		{
+			++variable_no;
+			sym.var_no = variable_no;
+		}
+		return tables.back().insert({name, sym}).second;
 	}
 
-	std::shared_ptr<Type> get(std::string name) const
+	const Symbol *get(const std::string &name) const
 	{
-		for (const Env *p = this; p != nullptr; p = p->prev.get())
+		for (auto it_table = tables.crbegin(); it_table != tables.crend(); ++it_table)
 		{
-			auto it = p->table.find(name);
-			if (it != p->table.end()) return it->second;
+			auto it = it_table->find(name);
+			if (it != it_table->end()) return &it->second;
 		}
 		return nullptr;
 	}
